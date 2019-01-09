4ecfa711f8bf7506ad135c5558b1849e469ce373
kalongenuj <sokalong@SOdeMacBook-Air-2.local>
2019-01-09 17:13:16 +0800

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser'); //take the JSON and convert it into an 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -34,6 +35,29 @@ app.get('/todos', (req, res)=>{
     }, (err)=>{
         res.status(400).send(err);
     });
+});
+
+//GET /todos/(individual items)url parameters
+app.get('/todos/:id', (req, res)=>{
+    var id = req.params.id;
+
+    //valid id using invalid
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    //findByID
+    Todo.findById(id).then((todo)=>{
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((err)=>{
+        res.status(400).send();
+    });
+    //sucess
+    //error
+
 });
 
 app.listen(3000, ()=>{
