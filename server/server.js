64ccc3481bc6823a517739b680804bd41c5aed63
kalongenuj <sokalong@SOdeMacBook-Air-2.local>
2019-01-10 01:06:14 +0800

    add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var bodyParser = require('body-parser'); //take the JSON and convert it into an OBJ
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser'); //take the JSON and convert it into an OBJ
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-var {ObjectID} = require('mongodb');
+const {ObjectID} = require('mongodb');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -73,6 +74,32 @@ app.delete('/todos/:id', (req,res)=>{
         }
         res.status(200).send({todo});
     }).catch((e)=>{
+        res.status(400).send();
+    })
+});
+
+//update
+app.patch('/todos/:id', (req,res) =>{
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']); //pick these two prop to body
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime(); //return JS timestamp
+
+    }else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+    Todo.findByIdAndUpdate(id, {$set:body}, {new: true}).then((todo)=>{
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch(()=>{
         res.status(400).send();
     })
 });
