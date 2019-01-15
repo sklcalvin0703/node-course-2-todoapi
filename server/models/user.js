4b9e292bbe64bfd2beb4e37b873411fa34614ecf
kalongenuj <sokalong@SOdeMacBook-Air-2.local>
2019-01-16 00:27:32 +0800

    Add post /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -73,6 +73,25 @@ UserSchema.statics.findByToken = function(token){
     });
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+    var user = this;
+    return user.findOne({email}).then((user)=>{
+        if((!user)){
+            return Promise.reject('no user found');
+        }
+        return new Promise((resolve,reject)=>{
+            bcrypt.compare(password, user.password, (err, res)=>{
+                if(res){
+                    resolve(user);
+                }else{
+                    reject('wrong pw');
+                }
+            })
+        })
+        
+});
+};
+
 UserSchema.pre('save', function (next){
     var user = this;
     if(user.isModified('password')){
