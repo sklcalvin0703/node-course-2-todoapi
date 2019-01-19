8ffd230861475def1db120f10a8ab074bc7742e6
spencer so <spencerso@spencerde-MacBook-Air.local>
2019-01-19 18:51:31 +0800

    test to use github

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -10,7 +10,7 @@ var authenticate = (req,res,next) =>{
         req.token = token;
         next();
     }).catch((e)=> {
-        res.status(401).send()
+        res.status(400).send()
     });
 };
 
